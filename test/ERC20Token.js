d00e400f2199eb781ce7608084c794b39736c7d6
mr-fool <mr-fool@users.noreply.github.com>
2019-04-05 10:55:08 -0600

    separated the test

--- test/ERC20Token.js
+++ test/ERC20Token.js
@@ -6,6 +6,11 @@ contract("sets the total supply upon deployment", async accounts => {
       let instance = await ERC20Token.deployed();
       let balance = await instance.totalSupply.call();
       assert.equal(balance.toNumber(), 1000000,'sets the total supply to 1,000,000')
+      
+    });
+    it("test msg.sender balance", async() =>{
+      let instance = await ERC20Token.deployed();
+      let balance = await instance.totalSupply.call();
       let adminBalance = await instance.balanceOf.call(accounts[0]);
       assert.equal(adminBalance.toNumber(),1000000,'testing msg.sender balance');
     });
