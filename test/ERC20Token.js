4606d0b30bc72dee7b1b0795d240ff0e0e543ea2
mr-fool <mr-fool@users.noreply.github.com>
2019-04-06 20:09:29 -0600

    test beforeEach()

--- test/ERC20Token.js
+++ test/ERC20Token.js
@@ -1,16 +1,16 @@
 var ERC20Token = artifacts.require("ERC20Token");
 
+beforeEach( async () => {
+  let instance = await ERC20Token.deployed();
 
-contract("sets the total supply upon deployment", async accounts => {
+});
+describe("sets the total supply upon deployment", async accounts => {
     it("sets the total supply upon deployment", async () => {
-      let instance = await ERC20Token.deployed();
       let balance = await instance.totalSupply.call();
       assert.equal(balance.toNumber(), 1000000,'sets the total supply to 1,000,000')
       
     });
     it("test msg.sender balance", async() =>{
-      let instance = await ERC20Token.deployed();
-      let balance = await instance.totalSupply.call();
       let adminBalance = await instance.balanceOf.call(accounts[0]);
       assert.equal(adminBalance.toNumber(),1000000,'testing msg.sender balance');
     });
