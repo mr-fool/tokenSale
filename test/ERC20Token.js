86eebfcd88ce3a29b8114773895e0b26c5f91de4
mr-fool <mr-fool@users.noreply.github.com>
2019-04-09 12:37:35 -0600

    added describe statement

--- test/ERC20Token.js
+++ test/ERC20Token.js
@@ -7,14 +7,16 @@ beforeEach( async () => {
   instance = await ERC20Token.deployed();
 
 });
-contract("sets the total supply upon deployment", async accounts => {
-    it("sets the total supply upon deployment", async () => {
-      let balance = await instance.totalSupply.call();
-      assert.equal(balance.toNumber(), 1000000,'sets the total supply to 1,000,000')
-      
-    });
-    it("test msg.sender balance", async() =>{
-      let adminBalance = await instance.balanceOf.call(accounts[0]);
-      assert.equal(adminBalance.toNumber(),1000000,'testing msg.sender balance');
-    });
+contract("ERC20", async accounts => {
+    describe('initial testing', () => {
+      it("sets the total supply upon deployment", async () => {
+        let balance = await instance.totalSupply.call();
+        assert.equal(balance.toNumber(), 1000000,'sets the total supply to 1,000,000')
+        
+      });
+      it("test msg.sender balance", async() =>{
+        let adminBalance = await instance.balanceOf.call(accounts[0]);
+        assert.equal(adminBalance.toNumber(),1000000,'testing msg.sender balance');
+      });
+  });
 });
