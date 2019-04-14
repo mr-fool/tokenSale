7cf246d18604cdea7fdd28ddc260d35d05998200
mr-fool <mr-fool@users.noreply.github.com>
2019-04-13 20:09:38 -0600

    Update ERC20Token.sol

--- contracts/ERC20Token.sol
+++ contracts/ERC20Token.sol
@@ -58,6 +58,7 @@ contract ERC20Token {
     }  
   function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
     require(_value <= balanceOf[_from]);
+    require(_value <= allowance[_from][msg.sender]);
     return true;
   }
 }
