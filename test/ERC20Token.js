727051e7b882b79bda29c98c766469a0fa0c2df1
mr-fool <mr-fool@users.noreply.github.com>
2019-04-19 15:14:18 -0600

    changed

--- test/ERC20Token.js
+++ test/ERC20Token.js
@@ -51,7 +51,7 @@ contract("ERC20", async accounts => {
       let approval =  await instance.approve.call(accounts[1], 100);
       assert.equal(approval, true, 'it returns true');
     });
-    
+
     it("testing allowance", async() => {
       let allowanceTest = await instance.allowance(accounts[0], accounts[1]);
       assert.equal(allowanceTest.toNumber(), 100, 'stores the allowance for delegated transfer');
@@ -60,7 +60,7 @@ contract("ERC20", async accounts => {
 
   describe('transfer from', () => {
     it("properly rejects invalid balance", async() => {
-      assert.equal(balance[accounts[1] <= 100, "right account");
+      assert.equal(balance(accounts[1]) <= 100, "right account");
       
     });
 
