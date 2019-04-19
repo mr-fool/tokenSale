ed878f37c06be4288c2bd92bfbe5a21f421c9517
mr-fool <mr-fool@users.noreply.github.com>
2019-04-19 17:12:34 -0600

    push

--- test/ERC20Token.js
+++ test/ERC20Token.js
@@ -60,8 +60,8 @@ contract("ERC20", async accounts => {
   describe('transfer from', () => {
     it("test allotted balance", async() => {
       await instance.transferFrom(accounts[0],accounts[1],100, {from : accounts[1]});
-      let accountZeroBalance = await instance.balanceOf(accounts[0]);
-      assert.equal(accountZeroBalance.toNumber(), 100, "right amount");
+      let accountZeroBalance = await instance.balanceOf(accounts[1]);
+      assert.equal(accountZeroBalance.toNumber(), 150, "right amount");
     });
 
   });
