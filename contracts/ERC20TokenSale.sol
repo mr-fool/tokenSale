cffdbe5a6b411c00b154d7ee6db7598ddb80b6c0
mr-fool <mr-fool@users.noreply.github.com>
2019-04-21 01:05:24 -0600

    added migration

---
+++ contracts/ERC20TokenSale.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.5.0;
+
+contract ERC20TokenSale {
+    constructor() public{
+        //Assign an admin
+        //Token Contract
+        //Token Price
+    }
+}
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 const ERC20Token = artifacts.require("ERC20Token");
+const ERC20Token = artifacts.require("ERC20TokenSale");
 
 module.exports = function(deployer) {
   deployer.deploy(ERC20Token,1000000);
+  deployer.deploy(ERC20TokenSale);
 };
