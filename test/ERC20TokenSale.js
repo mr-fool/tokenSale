38b7511f55b97df97ca2a8ac380bbe65177d971f
mr-fool <mr-fool@users.noreply.github.com>
2019-04-23 00:32:10 -0600

    more tests

--- test/ERC20TokenSale.js
+++ test/ERC20TokenSale.js
@@ -1,5 +1,14 @@
 const ERC20Token = artifacts.require("ERC20TokenSale");
 
-/*contract('ERC20TokenSale', function(accounts){
-    var tokenSaleInstance
-});*/
+beforeEach( async () => {
+    instance = await ERC20Token.deployed();
+  
+  });
+contract('ERC20TokenSale', function(accounts){
+    describe('constructor', () => {
+        it('initializes the contract with the correct values', async () => {
+            let address = await instance.address;
+            assert.notEqual(address, 0x0, 'has contract address');
+        });
+    });
+});
