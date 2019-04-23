40660a4c576a62490b18e864d0fcd0137b46d49e
mr-fool <mr-fool@users.noreply.github.com>
2019-04-23 17:48:36 -0600

    updated contract

--- contracts/ERC20TokenSale.sol
+++ contracts/ERC20TokenSale.sol
@@ -1,11 +1,14 @@
 pragma solidity ^0.5.0;
 
 import "./ERC20Token.sol";
+import "./SafeMath.sol";
 
 contract ERC20TokenSale {
+    using SafeMath for uint256;
     address admin;
     ERC20Token public tokenContract;
     uint256 public tokenPrice;
+    uint256 public tokensSold;
 
     constructor(ERC20Token _tokenContract, uint256 _tokenPrice) public{
         //Assign an admin
@@ -20,8 +23,9 @@ contract ERC20TokenSale {
         //Require that value is equal to tokens
         //Require that the contract has enough tokens
         //Require that a transfer is successful
-        
+
         //Keep track of tokens sold
+        tokensSold = tokensSold.add(_numberOfTokens);
         //Trigger Sell event
     }
 }
