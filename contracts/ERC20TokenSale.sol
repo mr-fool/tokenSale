d9391177beeaa00b8b17fb6af0e456abc534a3b5
mr-fool <mr-fool@users.noreply.github.com>
2019-04-22 18:04:59 -0600

    token sale

--- contracts/ERC20TokenSale.sol
+++ contracts/ERC20TokenSale.sol
@@ -1,6 +1,8 @@
 pragma solidity ^0.5.0;
 
 contract ERC20TokenSale {
+    address admin;
+    
     constructor() public{
         //Assign an admin
         //Token Contract
---
+++ test/ERC20TokenSale.js
@@ -0,0 +1,5 @@
+const ERC20Token = artifacts.require("ERC20TokenSale");
+
+/*contract('ERC20TokenSale', function(accounts){
+    var tokenSaleInstance
+});*/
