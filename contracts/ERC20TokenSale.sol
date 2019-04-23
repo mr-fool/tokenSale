414634a4ff59bf819a6477048af055cbb96d5003
mr-fool <mr-fool@users.noreply.github.com>
2019-04-23 01:40:01 -0600

    added tokenPrice

--- contracts/ERC20TokenSale.sol
+++ contracts/ERC20TokenSale.sol
@@ -5,12 +5,14 @@ import "./ERC20Token.sol";
 contract ERC20TokenSale {
     address admin;
     ERC20Token public tokenContract;
+    uint256 public tokenPrice;
 
-    constructor(ERC20Token _tokenContract) public{
+    constructor(ERC20Token _tokenContract, uint256 _tokenPrice) public{
         //Assign an admin
         admin = msg.sender;
         //Token Contract
         tokenContract = _tokenContract;
         //Token Price
+        tokenPrice = _tokenPrice;
     }
 }
--- test/ERC20TokenSale.js
+++ test/ERC20TokenSale.js
@@ -1,17 +1,29 @@
 const ERC20Token = artifacts.require("ERC20Token");
 const ERC20TokenSale = artifacts.require("ERC20TokenSale");
 
+let instance;
+let tokenPrice;
+
 beforeEach( async () => {
     instance = await ERC20TokenSale.deployed();
-  
+    tokenPrice = 1000000000000000; // in wei 0.001 ether
   });
 contract('ERC20TokenSale', function(accounts){
     describe('constructor', () => {
-        it('initializes the contract with the correct values', async () => {
+        it('has contract address', async () => {
             let address = await instance.address;
             assert.notEqual(address, 0x0, 'has contract address');
-            let tokenContractAddress = await instance.tokenContract();
-            assert.notEqual(tokenContractAddress, 0x0, 'has token contract address');
         });
+
+        it('has token contract address', async () => {
+            let tokenContractAddress = await instance.tokenContract();
+            assert.notEqual(tokenContractAddress, 0x0, 'has token contract address');  
+        });
+
+        it('token price is correct', async () => {
+            let price = await instance.tokenPrice();
+            assert.equal(price, tokenPrice, 'token price is correct');
+        });
+
     });
 });
