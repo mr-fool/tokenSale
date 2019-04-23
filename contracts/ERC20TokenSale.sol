416b1a54783dafc4842bfdfa8f1d383a4aa240ac
mr-fool <mr-fool@users.noreply.github.com>
2019-04-22 18:11:36 -0600

    Update ERC20TokenSale.sol

--- contracts/ERC20TokenSale.sol
+++ contracts/ERC20TokenSale.sol
@@ -1,10 +1,14 @@
 pragma solidity ^0.5.0;
 
+import "./ERC20Token.sol";
+
 contract ERC20TokenSale {
     address admin;
-    
+    ERC20Token public tokenContract;
+
     constructor() public{
         //Assign an admin
+        admin = msg.sender;
         //Token Contract
         //Token Price
     }
