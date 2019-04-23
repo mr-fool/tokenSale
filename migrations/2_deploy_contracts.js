c578305a58ad35144f183e3ac3c43b8429e8a229
mr-fool <mr-fool@users.noreply.github.com>
2019-04-23 00:41:00 -0600

    more tests

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,15 +1,12 @@
 const ERC20Token = artifacts.require("ERC20Token");
 const ERC20TokenSale = artifacts.require("ERC20TokenSale");
 
-
-
-/*module.exports = function(deployer) {
-  deployer.deploy(ERC20Token,1000000).then(function() {
-    return deployer.deploy(ERC20TokenSale, ERC20Token.address);
-  });
-
-};*/
-module.exports = async function(deployer) {
+/*module.exports = async function(deployer) {
   let deployCrowdTestTokenResult = await deployer.deploy(ERC20Token,1000000);
   let deployCrowdSaleResult = await deployer.deploy(ERC20TokenSale, ERC20Token.address);
-};;
+};*/
+module.exports = function(deployer) {
+  deployer.deploy(ERC20Token, 1000000).then(function() {
+    return deployer.deploy(ERC20TokenSale, ERC20Token.address);
+  });
+};
--- test/ERC20TokenSale.js
+++ test/ERC20TokenSale.js
@@ -9,6 +9,8 @@ contract('ERC20TokenSale', function(accounts){
         it('initializes the contract with the correct values', async () => {
             let address = await instance.address;
             assert.notEqual(address, 0x0, 'has contract address');
+            let tokenContractAddress = await instance.tokenContract();
+            assert.notEqual(tokenContractAddress, 0x0, 'has token contract address');
         });
     });
 });
