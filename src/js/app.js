c5daf85c7a3c7c9dfa58198961b09f0fa9793492
mr-fool <mr-fool@users.noreply.github.com>
2019-05-15 23:12:23 -0600

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -16,10 +16,15 @@ App = {
             App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
             web3 = new Web3(App.web3Provider);
       }
+      return App.initContracts();
     },
     initContracts: function() {
-        $.getJSON("ERC20Sale.json", function(ERC20TokenSale){
-            App.contracts.ERC20TokenSale = TruffleContract();
+        $.getJSON("ERC2TokenSale.json", function(ERC20TokenSale){
+            App.contracts.ERC20TokenSale = TruffleContract(ERC20TokenSale);
+            App.contracts.ERC20TokenSale.setProvider(App.web3Provider);
+            App.contracts.ERC20TokenSale.deployed().then(function(ERC20TokenSale){
+                console.log("ERC20 Token Sale Address:", ERC20TokenSale.address);
+            })
         })
     }
 }
