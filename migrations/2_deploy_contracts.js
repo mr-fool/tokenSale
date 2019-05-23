c660415e42598cc9781b54a441d8b26f77a6ebce
mr-fool <mr-fool@users.noreply.github.com>
2019-05-23 10:27:16 -0600

    out of gas

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,12 +1,12 @@
-const ERC20Token = artifacts.require("ERC20Token");
-const ERC20TokenSale = artifacts.require("ERC20TokenSale");
+const ERC20Token = artifacts.require("./ERC20Token");
+const ERC20TokenSale = artifacts.require("./ERC20TokenSale");
 
 /*module.exports = async function(deployer) {
   let deployCrowdTestTokenResult = await deployer.deploy(ERC20Token,1000000);
   let deployCrowdSaleResult = await deployer.deploy(ERC20TokenSale, ERC20Token.address);
 };*/
 module.exports = function(deployer) {
-  deployer.deploy(ERC20Token, 2000000).then(function() {
+  deployer.deploy(ERC20Token, 7000000).then(function() {
     let tokenPrice = 1000000000000000; // in wei 0.001 ether
     return deployer.deploy(ERC20TokenSale, ERC20Token.address,tokenPrice);
   });
